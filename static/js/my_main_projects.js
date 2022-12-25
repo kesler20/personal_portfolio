829e40981b81b400a23243b0d25fe1bed5f8770c
kesler20 <uchekesla@gmail.com>
2022-12-25 20:00:30 +0000

    the page is starting to look alright

--- static/js/my_main_projects.js
+++ static/js/my_main_projects.js
@@ -1,18 +1,60 @@
-let nextButton = document.querySelector(".next-btn")
-let projectsCards = Array.from(document.querySelector(".projects-cards").children)
+const projectsCards = Array.from(
+  document.querySelector(".projects-cards").children
+);
+
+// if mobile view hide the other two starting cards
+if (window.innerWidth <= 360) {
+  projectsCards[1].classList.add("hide");
+  projectsCards[2].classList.add("hide");
+}
+
 // remove the next button from the array
-projectsCards.pop()
-let currentCardInView = 0
+const nextButton = projectsCards.pop();
+const nextButtonIcon = nextButton.children.item(0);
+let nextButtonIconFlipped = true;
+
+let currentCardInView = 0;
 
 nextButton.addEventListener("click", () => {
-  currentCardInView++
-  if (currentCardInView != 0) {
-    projectsCards[currentCardInView - 1].classList.add("hide")
-  }
-  if (currentCardInView == projectsCards.length) {
-    projectsCards[0].classList.remove("hide")
-    currentCardInView = 0
+  // if the view is mobile change one card at a time
+  if (window.innerWidth <= 360) {
+    currentCardInView++;
+    if (currentCardInView != 0) {
+      projectsCards[currentCardInView - 1].classList.add("hide");
+    }
+    if (currentCardInView == projectsCards.length) {
+      // if the current card in view corresponds to the last one
+      // uncover the first element to restart, (the last one will be hidden from the line above)
+      projectsCards[0].classList.remove("hide");
+      currentCardInView = 0;
+      // flip the arrow
+      nextButtonIcon.classList.toggle("flip-right", nextButtonIconFlipped);
+      nextButtonIcon.classList.toggle("flip-left", !nextButtonIconFlipped);
+      nextButtonIconFlipped = !nextButtonIconFlipped;
+    } else {
+      projectsCards[currentCardInView].classList.remove("hide");
+    }
   } else {
-    projectsCards[currentCardInView].classList.remove("hide")
+    // flip the arrow
+    nextButtonIcon.classList.toggle("flip-right", nextButtonIconFlipped);
+    nextButtonIcon.classList.toggle("flip-left", !nextButtonIconFlipped);
+    nextButtonIconFlipped = !nextButtonIconFlipped;
+    if (currentCardInView == 0) {
+      projectsCards[0].classList.add("hide");
+      projectsCards[1].classList.add("hide");
+      projectsCards[2].classList.add("hide");
+      projectsCards[3].classList.remove("hide");
+      projectsCards[4].classList.remove("hide");
+      projectsCards[5].classList.remove("hide");
+      currentCardInView = 1;
+    } else {
+      projectsCards[0].classList.remove("hide");
+      projectsCards[1].classList.remove("hide");
+      projectsCards[2].classList.remove("hide");
+      projectsCards[3].classList.add("hide");
+      projectsCards[4].classList.add("hide");
+      projectsCards[5].classList.add("hide");
+      currentCardInView = 0;
+    }
   }
-})
+});
