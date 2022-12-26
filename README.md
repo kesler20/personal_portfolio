01ef1af9c8d76b8cbafb7e3bbf64dde392c828d9
kesler20 <uchekesla@gmail.com>
2022-12-26 02:40:18 +0000

    added a readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Overview
+
+the static folder contain all the static assets
+the css folder contains:
+- style.css : used for the general style of the entire website 
+in particular for the base.html template
+- animations.css : this is where all the animations are defined
+- (page_name).css @ each page has its own css file
+
+
+## TODO
+
+- [ ] rename all the css files to the corresponding pages
+- [ ] remove all the dead code
+- [ ] make the contact section functional
+- [ ] make the resizing of the content more professional
+- [ ] fix the content of eac page
+- [ ] update all the projects with the new ones
+- [ ] polish the website of the other projects
+- [ ] make sure that each link works
+- [ ] add more custom animations with scroll on view and active states
+- [ ] deploy on IONOS
+
+The new projects include:
+iGEM bioreactor
+iGEM toolbox
+polished isokoin
+drawUML
+sofia
+sofia-diet
+jaguar
+protocol-backend (system automation)
+lab
--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import redirect, url_for, render_template, request, session, flash
-from _database_model import *
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+import os
+from flask_sqlalchemy import SQLAlchemy
 
+ROOT_DIR = os.path.dirname(os.getcwd())
+app = Flask(
+    __name__, 
+    template_folder='templates',
+    static_folder='static'
+)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///isokoin.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'top secret!'
+app.secret_key = 'password'
+app.permanent_session_lifetime = timedelta(minutes=50)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
@@ -10,26 +24,6 @@ def home():
 @app.route('/<page>/', methods=['POST','GET'])
 def show(page):
     return render_template(f'{page}.html')
-
-@app.route('/thankyou', methods=['POST','GET'])
-def contact_me():
-    if request.method == 'POST':
-        service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-        emailMsg = request.form['msg']
-        mimeMessage = MIMEMultipart()
-        mimeMessage['to'] = request.form['email']
-        try:
-            mimeMessage['subject'] = request.form['subject']
-        except:
-            mimeMessage['subject'] = f"{request.form['name']}, {request.form['company']}"
-        mimeMessage.attach(MIMEText(emailMsg, 'plain'))
-        raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
-
-        message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
-        print(message)
-        return render_template('thankyou.html', user=request.form['email'])
-    else:
-        return render_template('thankyou.html', user='None')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5500) 
