e8acd1e4f026e115f76be4bd7f19e4a4f70f33e8
kesler20 <uchekesla@gmail.com>
2022-12-26 19:25:15 +0000

    added email functionality

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import smtplib
+import yagmail
+from config import email_username, email_password
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 import os
@@ -23,7 +26,37 @@ def home():
 
 @app.route('/<page>/', methods=['POST','GET'])
 def show(page):
-    return render_template(f'{page}.html')
+    if page == "thankyou":
+        """
+        this is the form of request expected
+        
+        ```txt
+        ImmutableMultiDict([
+        ('name', 'kesler isoko'), 
+        ('company', 'student'), 
+        ('msg', 'ththththt'), 
+        ('email', 'kisoko1@sheffield.ac.uk')
+        ])
+        ```
+        the Immutable dict object can be accessed from the request.form
+        as such ``request.form['name']`` -> ``'kesler isoko'``
+        """
+        if request.method == "POST":
+            # Set the login credentials and email server
+            yag = yagmail.SMTP(email_username, email_password)
+            try:
+                yag.login()
 
+                # Set the recipient and email content
+                to = request.form['email']
+                subject = f"Message by {request.form['name']} from {request.form['company']}"
+                body = request.form['msg']
+
+                # Send the email
+                yag.send(to, subject, body)
+            except smtplib.SMTPAuthenticationError:
+                pass
+            
+    return render_template(f'{page}.html')
 if __name__ == '__main__':
     app.run(debug=True, port=5500) 
